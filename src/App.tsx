e3cc2739c6cb2196ca4690a7368cce267ea75d8b
roandayne <roan.dino@sence1.com>
2024-02-27 12:17:31 +0800

    Add router and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { useState } from "react";
-import api from "../axiosInstance";
+import { Routes, Route } from "react-router-dom";
+import TestPage from "./container/TestPage/TestPage";
 
 const App = () => {
-  const [name, setName] = useState<string>("");
-  const [successMessage, setSuccessMessage] = useState<string>("");
-  const handleClick = () => {
-    api
-      .post("/api/tests", { name: name })
-      .then((response) => {
-        setSuccessMessage(response.data.message);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-        alignContent: "center",
-        alignItems: "center",
-        width: "100%",
-      }}
-    >
-      <Typography variant="h1">TEST CONNECTION</Typography>
-      <TextField
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-        id="outlined-basic"
-        label="Enter test name"
-        variant="outlined"
-      />
-      <Button onClick={handleClick} variant="outlined">
-        Submit
-      </Button>
-      {successMessage && <Typography>{successMessage}</Typography>}
-    </Box>
+    <Routes>
+      <Route path="/test" element={<TestPage />} />
+    </Routes>
   );
 };
 
