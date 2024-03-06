0059ed65798e1db3331203425a390f25afe34570
roandayne <roan.dino@sence1.com>
2024-03-06 17:27:59 +0800

    Update responsiveness

--- src/components/Introduction/IntroItem.tsx
+++ src/components/Introduction/IntroItem.tsx
@@ -1,26 +1,53 @@
+import { Card, CardMedia, Typography } from "@mui/material";
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Typography,
-} from "@mui/material";
 
-interface IntroItemProps {
-  title: string;
-  desc: string;
-  image: string;
-}
-
-const IntroItem: React.FC<IntroItemProps> = ({ title, desc, image }) => {
+const IntroItem: React.FC<IntroItem> = ({ title, desc, image }) => {
   return (
-    <Card>
-      <CardMedia component="img" alt="" height="140" image={image} />
-      <CardHeader>
-        <Typography variant="h3">{title}</Typography>
-      </CardHeader>
-      <CardContent>{desc}</CardContent>
+    <Card
+      sx={{
+        margin: "auto",
+        maxWidth: "calc((100% - 80px) / 3)",
+        minWidth: "340px",
+        padding: "20px",
+        minHeight: "460px",
+        borderRadius: "16px",
+        display: "flex",
+        flexDirection: "row",
+        boxShadow: "2px 4px rgba(245, 138, 71, 0.22)",
+        backgroundColor: "#bcd2ff",
+        flexWrap: "wrap",
+      }}
+    >
+      <CardMedia
+        component="img"
+        alt=""
+        height="240"
+        image={image}
+        sx={{ mb: 1 }}
+      />
+      <Typography
+        variant="h3"
+        align="center"
+        sx={{
+          mb: 2,
+          fontFamily: "Poppins",
+          fontWeight: 600,
+          fontSize: "24px",
+          color: "#d2580b",
+          lineHeight: 1.25,
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography
+        variant="body2"
+        sx={{
+          fontFamily: "Open Sans",
+          fontSize: "16px",
+        }}
+      >
+        {desc}
+      </Typography>
     </Card>
   );
 };
--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,22 +1,42 @@
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 import IntroItem from "./IntroItem";
-import { Box, Container, Typography } from "@mui/material";
 
 const Introduction: React.FC<IntroProps> = ({ introduction }) => {
   return (
-    <Box component="section" id="introduction">
-      <Container fixed>
-        <Typography variant="h3">Introduction to Currante</Typography>
-        {introduction.map((introItem: IntroItem, index: number) => {
-          return (
-            <IntroItem
-              key={index}
-              title={introItem.title}
-              desc={introItem.desc}
-              image={introItem.image}
-            />
-          );
-        })}
+    <Box
+      sx={{
+        padding: { xs: "62px 0", md: "62px 97px" },
+        width: "100%",
+        backgroundColor: "#d8e5ff",
+        textAlign: { xs: "center", md: "left" },
+      }}
+    >
+      <Container>
+        <Typography
+          variant="h2"
+          sx={{
+            fontFamily: "Poppins",
+            fontWeight: 700,
+            fontSize: "48px",
+            color: "#0e2f71",
+            marginBottom: "75px",
+          }}
+        >
+          Introduction to Currante
+        </Typography>
+        <Box sx={{ display: "flex", flexWrap: "wrap", gap: "40px" }}>
+          {introduction.map((introItem: IntroItem, index: number) => {
+            return (
+              <IntroItem
+                key={index}
+                title={introItem.title}
+                desc={introItem.desc}
+                image={introItem.image}
+              />
+            );
+          })}
+        </Box>
       </Container>
     </Box>
   );
