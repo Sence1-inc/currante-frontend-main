db128690725dcaeb3b18de40fe2b4c539b555c1c
CRH-ggalinato <g.galinato@creativehope.co.jp>
2024-03-06 15:50:14 +0800

    develop-feature/teaser-main-content-ui: adding additional fixes to keyvisual, header, footer - adding additional buttons

--- src/components/Buttons/PrimaryButton/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,35 +1,37 @@
-// import { Link as RouterLink } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 
 interface PrimaryButtonProps {
   text: string;
   url?: string;
-  fullWidth?: boolean
+  fullWidth?: boolean;
 }
 
-const PrimaryButton: React.FC<PrimaryButtonProps> = ({ text, url, fullWidth }) => {
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({
+  text,
+  url,
+  fullWidth,
+}) => {
   return (
-    <Button 
-      sx={
-        { py: 2, 
-          px: 6,
-          borderRadius: 4,
-          backgroundColor: 'secondary.main', 
-          color: 'common.white',
-          variant: 'h6',
-          textTransform: 'uppercase',
-          fontWeight: 'bold',
-          '&:hover': {
-            backgroundColor: 'secondary.dark',
-          },
-        }
-      }
+    <Button
+      sx={{
+        py: 2,
+        px: 6,
+        borderRadius: 4,
+        backgroundColor: "secondary.main",
+        color: "common.white",
+        variant: "h6",
+        textTransform: "uppercase",
+        fontWeight: "bold",
+        "&:hover": {
+          backgroundColor: "secondary.dark",
+        },
+      }}
       fullWidth={fullWidth}
       href={url}
     >
-      { text }
+      {text}
     </Button>
-  )
-}
+  );
+};
 
 export default PrimaryButton;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import { createTheme, Theme, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { createTheme, Theme, ThemeProvider } from "@mui/material";
 
 const theme: Theme = createTheme({
   breakpoints: {
