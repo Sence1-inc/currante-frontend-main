2e7debf00f58227531203f546237aee739c6a65e
roandayne <roan.dino@sence1.com>
2024-03-08 05:49:54 +0800

    Add explore our services

---
+++ src/data/ExploreOurServicesContent.tsx
@@ -0,0 +1,44 @@
+export const SERVICES: ServiceItem[] = [
+  {
+    order: 1,
+    service: "plumber",
+    description:
+      "Dealing with plumbing issues? Have peace of mind and a seamlessly operating home. Book an inspection now!",
+    image: "/images/img_services_plumbing_1.jpg",
+  },
+  {
+    order: 3,
+    service: "plumber",
+    description:
+      "From simple leak repairs to fixture installations, trust us to handle all your plumbing needs with expertise and care.",
+    image: "/images/img_services_plumbing_2.jpg",
+  },
+  {
+    order: 4,
+    service: "carpenter",
+    description:
+      "Planning a major home project? Schedule your inspection today for expert carpentry assistance.",
+    image: "/images/img_services_carpentry_1.jpg",
+  },
+  {
+    order: 5,
+    service: "carpenter",
+    description:
+      "Unlock the potential of your space! Experience top-notch carpentry solutions tailored to your short-term goals.",
+    image: "/images/img_services_carpentry_2.jpg",
+  },
+  {
+    order: 6,
+    service: "cleaner",
+    description:
+      "Finishing touches matter! Elevate your space with our swift and expert post-construction cleaning services.",
+    image: "/images/img_services_cleaning_1.jpg",
+  },
+  {
+    order: 7,
+    service: "cleaner",
+    description:
+      "Refresh your space in a flash! Experience the sparkle with our efficient general cleaning services.",
+    image: "/images/img_services_cleaning_2.jpg",
+  },
+];
