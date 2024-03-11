e0617efc52e97e27aaf505dc690f3f9f734f266b
roandayne <roan.dino@sence1.com>
2024-03-11 19:42:09 +0800

    Import images for better rendering

--- src/data/ExploreOurServicesContent.tsx
+++ src/data/ExploreOurServicesContent.tsx
@@ -1,44 +1,51 @@
+import plumber1 from "/images/img_services_plumbing_1.jpg";
+import plumber2 from "/images/img_services_plumbing_2.jpg";
+import carpenter1 from "/images/img_services_carpentry_1.jpg";
+import carpenter2 from "/images/img_services_carpentry_2.jpg";
+import cleaner1 from "/images/img_services_cleaning_1.jpg";
+import cleaner2 from "/images/img_services_cleaning_2.jpg";
+
 export const SERVICES: ServiceItem[] = [
   {
     order: 1,
     service: "plumber",
     description:
       "Dealing with plumbing issues? Have peace of mind and a seamlessly operating home. Book an inspection now!",
-    image: "/images/img_services_plumbing_1.jpg",
+    image: plumber1,
   },
   {
     order: 3,
     service: "plumber",
     description:
       "From simple leak repairs to fixture installations, trust us to handle all your plumbing needs with expertise and care.",
-    image: "/images/img_services_plumbing_2.jpg",
+    image: plumber2,
   },
   {
     order: 4,
     service: "carpenter",
     description:
       "Planning a major home project? Schedule your inspection today for expert carpentry assistance.",
-    image: "/images/img_services_carpentry_1.jpg",
+    image: carpenter1,
   },
   {
     order: 5,
     service: "carpenter",
     description:
       "Unlock the potential of your space! Experience top-notch carpentry solutions tailored to your short-term goals.",
-    image: "/images/img_services_carpentry_2.jpg",
+    image: carpenter2,
   },
   {
     order: 6,
     service: "cleaner",
     description:
       "Finishing touches matter! Elevate your space with our swift and expert post-construction cleaning services.",
-    image: "/images/img_services_cleaning_1.jpg",
+    image: cleaner1,
   },
   {
     order: 7,
     service: "cleaner",
     description:
       "Refresh your space in a flash! Experience the sparkle with our efficient general cleaning services.",
-    image: "/images/img_services_cleaning_2.jpg",
+    image: cleaner2,
   },
 ];
