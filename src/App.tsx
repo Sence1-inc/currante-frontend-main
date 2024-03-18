6a5661d8d5a13b70464d5354ab870279fc3d0eed
sence1-rachelle <rachelle.gregorio@sence1.com>
2024-03-18 09:55:36 +0800

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import HomePage from "./container/HomePage/HomePage";
 import JobListPage from "./container/JobListPage/JobListPage";
+import ProfilePage from "./container/ProfilePage/ProfilePage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/jobs" element={<PrivateRoute component={JobListPage} />} />
+      <Route
+        path="/profile"
+        element={<PrivateRoute component={ProfilePage} />}
+      />
     </Routes>
   );
 };
---
+++ src/container/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import { Box } from "@mui/material";
+
+
+const ProfilePage: React.FC = () => {
+    return (
+        <>
+        <Box>
+            <Header />
+            <Footer />
+        </Box>
+        </>
+    );
+};
+
+export default ProfilePage
