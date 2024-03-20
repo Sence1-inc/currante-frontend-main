67592609af221ebf833c67ffc6c2bfaf3ecf2012
sdidulosence1 <sarah.didulo@sence1.com>
2024-03-20 18:11:14 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./container/HomePage/HomePage";
+import WorkerHomePage from "./container/WorkerHomePage/WorkerHomePage";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
+import BottomNavigation from "./components/BottomNavigation/BottomNavigation";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <>
+      <TopNavigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/job-list" element={<WorkerHomePage />} />
+        </Routes>
+      <BottomNavigation />
+    </>
   );
 };
 
---
+++ src/container/WorkerHomePage/WorkerHomePage.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const WorkerHomePage: React.FC = () => {
+    return (
+      <Box>
+        
+        
+      </Box>
+    );
+  };
+  
+  export default WorkerHomePage;
