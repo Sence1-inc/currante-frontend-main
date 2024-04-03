1db6510bdab989a82f277ef44d1e32d2f6cc9655
roandayne <roan.dino@sence1.com>
2024-04-04 01:50:37 +0800

    Add checker for certain words

--- src/components/Chat/SendChat.tsx
+++ src/components/Chat/SendChat.tsx
@@ -2,7 +2,8 @@ import { collection, getDoc, getDocs, query, where } from "@firebase/firestore";
 import SendIcon from "@mui/icons-material/Send";
 import { Box, IconButton, TextField } from "@mui/material";
 import { addDoc, serverTimestamp } from "firebase/firestore";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { EXCLUDED_WORDS } from "../../data/ChatDetails";
 import { LOGGED_IN_USER } from "../../data/WorkerDetails";
 import { db } from "../../firebase";
 
@@ -12,29 +13,47 @@ interface SendChatProps {
 
 const SendChat: React.FC<SendChatProps> = ({ conversation_id }) => {
   const [chat, setChat] = useState<string>("");
+  const [isInvalid, setIsInvalid] = useState<boolean>(false);
 
-  const handleSendChat = async () => {
-    setChat("");
+  const hasExcludedWord = (): boolean => {
+    const words = chat.toLowerCase().split(/\s+/);
+    return EXCLUDED_WORDS.some((excludedWord) => words.includes(excludedWord));
+  };
 
-    try {
-      const userRef = query(
-        collection(db, "users"),
-        where("user_id", "==", LOGGED_IN_USER) // update during authentication implementation
-      );
-      const users = await getDocs(userRef);
-      const docRef = users.docs[0].ref;
+  useEffect(() => {
+    if (chat) {
+      setIsInvalid(hasExcludedWord());
+    }
+  }, [chat]);
 
-      const userDoc = await getDoc(docRef);
+  const handleSendChat = async (e: any) => {
+    const phoneRegex = /\b\d{10}\b|\b\d{3}[ -.]?\d{3}[ -.]?\d{4}\b/;
+    if (!phoneRegex.test(e.target.value) || !hasExcludedWord()) {
+      setIsInvalid(false);
+      setChat("");
 
-      await addDoc(collection(db, "messages"), {
-        body: chat,
-        user_id: LOGGED_IN_USER, // update this during implementation of authentication
-        conversation_id: conversation_id,
-        created_at: serverTimestamp(),
-        sender_id: userDoc.id,
-      });
-    } catch (error) {
-      console.log(error);
+      try {
+        const userRef = query(
+          collection(db, "users"),
+          where("user_id", "==", LOGGED_IN_USER) // update during authentication implementation
+        );
+        const users = await getDocs(userRef);
+        const docRef = users.docs[0].ref;
+
+        const userDoc = await getDoc(docRef);
+
+        await addDoc(collection(db, "messages"), {
+          body: chat,
+          user_id: LOGGED_IN_USER, // update this during implementation of authentication
+          conversation_id: conversation_id,
+          created_at: serverTimestamp(),
+          sender_id: userDoc.id,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setIsInvalid(true);
     }
   };
 
@@ -63,11 +82,11 @@ const SendChat: React.FC<SendChatProps> = ({ conversation_id }) => {
           },
         }}
         value={chat}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setChat(e.target.value)
-        }
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setChat(e.target.value);
+        }}
       />
-      <IconButton disabled={!chat} onClick={handleSendChat}>
+      <IconButton disabled={!chat || isInvalid} onClick={handleSendChat}>
         <SendIcon />
       </IconButton>
     </Box>
