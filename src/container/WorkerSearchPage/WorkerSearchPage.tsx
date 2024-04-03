5ef2165b85856e0a925c12ab71c6915f69ce86b2
sence1-rachelle <rachelle.gregorio@sence1.com>
2024-04-03 17:59:03 +0800

    added image slider

--- src/container/WorkerSearchPage/WorkerSearchPage.tsx
+++ src/container/WorkerSearchPage/WorkerSearchPage.tsx
@@ -8,6 +8,37 @@ import EventNoteIcon from '@mui/icons-material/EventNote';
 import StarIcon from '@mui/icons-material/Star';
 import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const spanStyle = {
+    padding: '20px',
+    background: '#efefef',
+    color: '#000000'
+  }
+  
+  const divStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundSize: 'cover',
+    height: '200px',
+    borderRadius: '28px'
+  }
+  const slideImages = [
+    {
+      url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      caption: 'Slide 1'
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+      caption: 'Slide 2'
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      caption: 'Slide 3'
+    },
+  ];
 
 const WorkerSearchPage: React.FC = () => {
 
@@ -20,6 +51,21 @@ const WorkerSearchPage: React.FC = () => {
                 margin: '100px auto'
             }}
         >
+            <Box 
+                sx={{
+                    padding: '20px 0'
+                }}
+                className="slide-container">
+                <Slide>
+                {slideImages.map((slideImage, index)=> (
+                    <div key={index}>
+                        <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+                            <span style={spanStyle}>{slideImage.caption}</span>
+                        </div>
+                    </div>
+                    ))} 
+                </Slide>
+            </Box>
             <Box
                 sx={{
                     background: '#A1B5DE',
@@ -152,6 +198,7 @@ const WorkerSearchPage: React.FC = () => {
             >
                 <Typography
                     sx={{
+                        margin: '10px 0 20px',
                         fontSize: '16px',
                         fontWeight: '500'
                     }}
@@ -162,10 +209,88 @@ const WorkerSearchPage: React.FC = () => {
                     sx={{
                         borderRadius: '12px',
                         padding: '10px 15px',
-                        background: '#C5C6D0'
+                        border: '1px solid #C5C6D0'
                     }}
                 >
+                    <Box
+                        display={'flex'}
+                        flexDirection={'row'}
+                        flexWrap={'wrap'}
+                        width={'100%'}
+                    >
+                        <img src="../public/images/img_worker_online.png" alt="placeholder" />
+                        <Box
+                            display={'flex'}
+                            flexDirection={'column'}
+                            flexBasis={'100%'}
+                            flex={'1'}
+                            justifyContent={'space-evenly'}
+                            alignItems={'center'}
+                            p={'0 0 10px'}
+                        >
+                            <Typography
+                                sx={{
+                                    fontSize: '16px',
+                                    fontWeight: '500'
+                                }}
+                            >
+                            Header
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontSize: '8px',
+                                    fontWeight: '400'
+                                }}
+                            >
+                                #123456
+                            </Typography>
+                        </Box>
 
+                        <Box
+                            display={'flex'}
+                            flexDirection={'column'}
+                            flexBasis={'100%'}
+                            flex={'1'}
+                            justifyContent={'space-evenly'}
+                            alignItems={'center'}
+                            p={'0 0 10px'}
+                        >
+                            <Typography
+                                sx={{
+                                    fontSize: '12px',
+                                    fontWeight: '400'
+                                }}
+                            >
+                            Jane Smith
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontSize: '12px',
+                                    fontWeight: '400'
+                                }}
+                            >
+                                Quezon City
+                            </Typography>
+                        </Box>
+                        <Box
+                            display={'flex'}
+                            flexDirection={'column'}
+                            flexBasis={'100%'}
+                            flex={'1'}
+                            justifyContent={'space-evenly'}
+                            alignItems={'center'}
+                            p={'0 0 10px'}
+                        >
+                            <Typography
+                                sx={{
+                                    fontSize: '8px',
+                                    fontWeight: '400'
+                                }}
+                            >
+                            Available schedule
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Box>
             </Box>
             
