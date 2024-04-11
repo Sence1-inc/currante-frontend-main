a59cad90bd931243c33057e8519debd4b9e840b3
roandayne <roan.dino@sence1.com>
2024-04-11 22:30:31 +0800

    Get user on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import axiosInstance from "../axiosInstance";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import HomePage from "./container/HomePage/HomePage";
 import JobListPage from "./container/JobListPage/JobListPage";
 import ProfilePage from "./container/ProfilePage/ProfilePage";
+import { LOGGED_IN_USER } from "./data/WorkerDetails";
+import { initializeUser } from "./redux/reducers/UserReducer";
+import { useAppDispatch } from "./redux/store";
 
 const App = () => {
+  // do this on log in
+  const dispatch = useAppDispatch();
+  const [id, setId] = useState<number>(LOGGED_IN_USER);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const { data } = await axiosInstance.get(`/api/v1/users/${id}`);
+        if (data) {
+          dispatch(initializeUser(data));
+        }
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    };
+
+    if (id) {
+      getUser();
+    }
+  }, [dispatch, id]);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
