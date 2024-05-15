3757b553f90390a4ce6502be4e9d48d8346cbead
sdidulosence1 <sarah.didulo@sence1.com>
2024-05-15 10:46:53 +0800

    retry commit

--- src/components/SliderText/SliderText.tsx
+++ src/components/SliderText/SliderText.tsx
@@ -7,22 +7,7 @@ const SliderText: React.FC = () => {
 
 
   return(
-    <Typography
-            variant="h1"
-            className="headingText" 
-            sx={{
-              textAlign: { md: "left", xs: "center" },
-              color: "primary.main",
-              whiteSpace: "pre-wrap",
-              fontFamily: "Open Sans",
-              fontSize: "27px",
-              lineHeight: 1.5,
-              mb: 4,
-            }}
-          >
-
-            {headingText}
-          </Typography>
+    
   );  
 };
 
