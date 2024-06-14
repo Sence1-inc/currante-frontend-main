e1aff3f89d473dd570cce3a083cfa16a028b87f8
roandayne <roan.dino@sence1.com>
2024-06-14 11:59:59 +0800

    Trigger refresh token endpoint

--- axiosInstance.ts
+++ axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import { Navigate } from "react-router";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
@@ -7,7 +8,7 @@ const api: AxiosInstance = axios.create({
   timeout: 10000,
   withCredentials: true,
   headers: {
-    "Content-Type": ["application/json", "*/*"],
+    "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH",
     "Access-Control-Allow-Headers": "*",
@@ -19,7 +20,7 @@ let lastRefreshTime = Date.now();
 
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
+  async (error) => {
     const currentTime = Date.now();
     const elapsedTimeSinceLastRefresh = currentTime - lastRefreshTime;
 
@@ -27,9 +28,24 @@ api.interceptors.response.use(
       elapsedTimeSinceLastRefresh >= 4 * 60 * 1000 ||
       (error.response && error.response.status === 401)
     ) {
-      axios.post(`${baseURL}/api/v1/refresh`);
-      console.log("Unauthorized, triggering /refresh endpoint");
-      lastRefreshTime = currentTime;
+      const data = {};
+      try {
+        const response = await axios.post(`${baseURL}/api/v1/refresh`, data, {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
+        console.log("Unauthorized, triggered /refresh endpoint", response.data);
+
+        lastRefreshTime = currentTime;
+
+        return api.request(error.config);
+      } catch (refreshError) {
+        console.error("Failed to refresh token", refreshError);
+        Navigate({ to: "/sign-in" });
+      }
     }
 
     return Promise.reject(error);
